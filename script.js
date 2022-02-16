16f7d5dc19d8140bfce2d7f9cc8168a1fd7bb9de
Gisele <gisele.viedenhelfen@gmail.com>
2022-02-15 22:08:05 -0300

    requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
  //código inspirado na resolução do exercício parte II do 5.3 js
  //https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-eventos-gabarito/solutions/700128ff-5150-4320-9b33-875c7e6d5a2e/gabarito-dos-exercicios/b1d700e5-2b66-48c3-8af0-310cb17eacda?use_case=calendar
 //requisito 7
- function cliqueCor(event){
+let classeSelecionada = 'green'
+function cliqueCor(event){
      const cores = document.querySelector('.selected');
      cores.classList.remove('selected');
     event.target.classList.add('selected');
+    classeSelecionada = document.getElementsByClassName('selected')[0]
  }
 
 
@@ -24,21 +26,20 @@ vermelho.addEventListener('click', cliqueCor)
 
 
 // requisito 8
-window.onload = function(){
-    let classeSelecionada = document.getElementsByClassName('color selected')[0]
-    let cor = classeSelecionada.innerText
+function trocarCor(){
     let quadradinhos= document.getElementsByClassName('pixel')
-    for(index=0; index<quadradinhos.length;index+=1){
-       
+    for(index=0; index<quadradinhos.length;index+=1){     
         let quadradoDaVez = quadradinhos[index]
         quadradoDaVez.addEventListener('click', function(event){
-            quadradoDaVez.style.backgroundColor= cor
+            classeSelecionada = document.getElementsByClassName('selected')[0]
+            let cor = classeSelecionada.innerText
+            console.log(classeSelecionada)
+            event.target.style.backgroundColor= cor
         })
     }
 }
-
-
-
+trocarCor()
+  
 
 //requisito 9
 const botaoReset= document.getElementById('clear-board')
